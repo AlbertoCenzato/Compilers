866ca7b1469a523289a4ab9d5330d4ce609f2c4e
cenz <albecenz@gmail.com>
2017-06-27 18:21:25 +0200

    Translation to C complete

--- src/backend.c
+++ src/backend.c
@@ -6,9 +6,10 @@
 
 #include "code_list.h"
 #include "tac.h"
-
+#include "fract.h"
 
 int count = 0;
+
 int alreadyDefined(char* c) {
 	int n = atoi(c + 1);
 	if (n > count) {
@@ -20,32 +21,35 @@ int alreadyDefined(char* c) {
 
 
 void printDeclaration(char* result, FILE *out) {
-	fprintf(out, "int %s;", result);
+	fprintf(out, "int %s", result);
 }
 
 void printAssignment(char* result, char* op1, char* oper, char* op2, FILE *out) {
-	if (!alreadyDefined(result))
-		fprintf(out, "int ");
-	fprintf(out, "%s = ", result);
+	//if (!alreadyDefined(result))
+	//	fprintf(out, "int ");
+	fprintf(out, "%s", result);
+	if (op1 == NULL && oper == NULL && op2 == NULL) { // it's a declaration
+		return;
+	}
+	fprintf(out, " = ");	// otherwise it's an assignment
 	if (op1 != NULL)
 		fprintf(out, "%s", op1);
 	if (oper != NULL)
 		fprintf(out, "%s", oper);
 	if (op2 != NULL)
 		fprintf(out, "%s", op2);
-	fprintf(out, ";");
 }
 
 void printMcd(char* result, char* op1, char* op2, FILE *out) {
-	fprintf(out, "%s = mcd(%s, %s);", result, op1, op2);
+	fprintf(out, "%s = mcd(%s, %s)", result, op1, op2);
 }
 
 void printGoto(char* op2, FILE *out) {
-	fprintf(out, "goto %s;", op2);
+	fprintf(out, "goto %s", op2);
 }
 
 void printGotoIfNZ(char* op1, char* op2, FILE *out) {
-	fprintf(out, "if (%s != 0) goto %s;", op1, op2);
+	fprintf(out, "if (%s != 0) goto %s", op1, op2);
 }
 
 void bkndPrintTac(TAC* tac, FILE* out) {
@@ -61,7 +65,9 @@ void bkndPrintTac(TAC* tac, FILE* out) {
 		}
 		fprintf(out, "%s: ", label);
 	}
-
+	if (result != NULL && !alreadyDefined(result)) {
+		fprintf(out, "int ");
+	}
 	if (oper != NULL) {
 		if (strcmp(oper, GEN_MCD) == 0)
 			printMcd(result, op1, op2, out);
@@ -72,23 +78,44 @@ void bkndPrintTac(TAC* tac, FILE* out) {
 		else
 			printAssignment(result, op1, oper, op2, out);
 	}
-	else if(op1 == NULL && op2 == NULL && oper == NULL)
-		printDeclaration(result, out);
+	//else if(op1 == NULL && op2 == NULL)
+	//	printDeclaration(result, out);
 	else
 		printAssignment(result, op1, oper, op2, out);
 
-	fprintf(out, "\n");
+	fprintf(out, ";\n");
+}
+
+void printMCD(FILE *out) {
+	fprintf(out, "int mcd(int n1, int n2) {\n");
+	fprintf(out, "   if (n2 == 0)\n");
+	fprintf(out, "      return n1;\n");
+	fprintf(out, "   else\n");
+	fprintf(out, "      return mcd(n2, n1%%n2);\n");
+	fprintf(out, "}\n\n");
 }
 
 /**
- *	\brief WARNING! by now this is a destructive call. 
+ *	\brief WARNING! by now this is a destructive call.
  *			 The list is printed and deallocated
  */
 void bkndPrintToC(CodeList* code, FILE *out) {
 	
+	fprintf(out, "#include <stdio.h>\n\n");
+
+	printMCD(out);
+
+	fprintf(out, "int main(int argc, char** argv) {\n");
+
 	while(listHasNext(code)) {
 		TAC* tac = listGetNext(code);
 		bkndPrintTac(tac, out);
 	}
+
+	char *num = fractNumFromList(code);
+	char *den = fractDenFromList(code);
+
+	fprintf(out, "printf(\"[%%d,%%d]\",%s, %s);\nreturn 0;\n}", num, den);
+
 	listFree(code);
 }
